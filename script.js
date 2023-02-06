d4c8f6ad06880e306eb1ea633c247c34edc865f1
Lorenzo Cappiello <108156870+lorenzocappiello@users.noreply.github.com>
2023-02-06 12:20:30 +0100

    Script Update

--- script.js
+++ script.js
@@ -95,3 +95,29 @@ let unmatch = function(){
     }, 500);
     count = 0;
 }
+
+/* Timer */ 
+
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+var timer = setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+function StopCount() {
+    clearTimeout(timer);
+}
